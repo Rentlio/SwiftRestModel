8b30e0c6d9026d193b9fc9047b3ea162d74979a3
Juraj Hilje <jhilje@gmail.com>
2016-01-28 09:41:12 +0100

    Cleanup boilerplate code

--- SwiftRestModel/ViewController.swift
+++ SwiftRestModel/ViewController.swift
@@ -1,25 +1,9 @@
-//
-//  ViewController.swift
-//  SwiftRestModel
-//
-//  Created by Juraj Hilje on 28/01/16.
-//  Copyright © 2016 Juraj Hilje. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
-
