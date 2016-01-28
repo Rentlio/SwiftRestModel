bf6933319e7dd60f5df7caafb10ad4057a0a0c6c
Juraj Hilje <jhilje@gmail.com>
2016-01-28 13:54:02 +0100

    Update example post model

--- Example/Models/Post.swift
+++ Example/Models/Post.swift
@@ -1,7 +1,9 @@
 class Post: SwiftRestModel {
     
+    let url = "http://jsonplaceholder.typicode.com/posts"
+    
     init(data: Dictionary<String, AnyObject> = [:]) {
-        super.init(rootUrl: "http://jsonplaceholder.typicode.com/posts", data: data)
+        super.init(rootUrl: self.url, data: data)
     }
     
 }
