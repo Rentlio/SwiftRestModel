4c287f4ad4e5a9aa4bdffabba84ffbc285239505
Juraj Hilje <jhilje@gmail.com>
2016-02-01 11:43:24 +0100

    Add tests for fetch method

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -25,9 +25,40 @@ class SwiftRestModelTests: XCTestCase {
         XCTAssertEqual(data.rawString(), model.data.rawString(), "parse method modified data")
     }
     
+    func testFetchNewModel() {
+        let expectation = self.expectationWithDescription("fetch posts")
+        
+        model.rootUrl = "http://jsonplaceholder.typicode.com/posts"
+        model.fetch(
+            success: {
+                response in
+                XCTAssertNotNil(self.model.data, "response is empty")
+                XCTAssertNotEqual(self.model.data.arrayValue.count, 0, "collection is empty")
+                expectation.fulfill()
+        })
+        
+        self.waitForExpectationsWithTimeout(5.0, handler: nil)
+    }
+    
+    func testFetchExistingModel() {
+        let expectation = self.expectationWithDescription("fetch posts")
+        
+        model.rootUrl = "http://jsonplaceholder.typicode.com/posts"
+        model.data["id"] = "1"
+        model.fetch(
+            success: {
+                response in
+                XCTAssertNotNil(self.model.data, "response is empty")
+                XCTAssertEqual(self.model.data.arrayValue.count, 0, "collection is not empty")
+                expectation.fulfill()
+        })
+        
+        self.waitForExpectationsWithTimeout(5.0, handler: nil)
+    }
+    
     func testIsNew() {
         XCTAssertTrue(model.isNew(), "model should be new")
-        model.data["id"] = "1"
+        model.data["id"] = "id1"
         XCTAssertFalse(model.isNew(), "model should not be new")
     }
     
